e4d3563a74be7ebc4f70fec8c487705abdfcbb75
usuario <usuario@usuario-PC>
2023-09-03 16:32:01 -0500

    Ejercicio #10

---
+++ src/actividad2/Ejercicio10.java
@@ -0,0 +1,24 @@
+package actividad2;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int numeroInscrpcion, estratoSocial;
+        String nombres;
+        double patrimonio;
+        double valorMatricula = 50000;
+
+        numeroInscrpcion = Teclado.inputInt("Ingrese el numero de inscripcion del estudiante: ");
+        nombres = Teclado.inputString("Ingrese el nombre del estudiante: ");
+        patrimonio = Teclado.inputDouble("Ingrese el patrimonio del estudiante: ");
+        estratoSocial = Teclado.inputInt("Ingrese el estrato social del estudiante: ");
+
+        if (patrimonio > 2000000 && estratoSocial > 3) {
+            valorMatricula += patrimonio * 0.03;
+        }
+
+        System.out.println("El estudiante con numero de inscripcion " + numeroInscrpcion + " y nombre " + nombres
+                + " debe pagar: $" + valorMatricula);
+
+    }
+
+}
