6813b9f10edb20a03c09aa5a051e4a8b5f73438b
usuario <usuario@usuario-PC>
2023-09-04 17:53:48 -0500

    Ejercicio#7

---
+++ src/actividad_2/Ejercicio_7.java
@@ -0,0 +1,20 @@
+package actividad_2;
+
+public class Ejercicio_7 {
+    public static void main(String[] args) {
+        int valorA, valorB;
+
+        valorA = Teclado.inputInt("Ingrese el valor A: ");
+        valorB = Teclado.inputInt("Ingrese el valor B: ");
+
+        if (valorA > valorB) {
+            System.out.println("A es mayor que B");
+        } else if (valorA < valorB) {
+            System.out.println("A es menor que B");
+        } else {
+            System.out.println("A es igual a B");
+        }
+
+    }
+
+}
